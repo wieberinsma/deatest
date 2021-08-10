35769a969d83570712839103761b562fd42c7859
Wiebe Rinsma <wiebe.rinsma@han.nl>
2021-08-10 21:14:57 +0200

    String Kata af

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,24 +1,119 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class StringCalculator
 {
-    private static final String DELIMITER = ",";
+    private static final String DELIMITER_REGEX = "^(?s)\\/\\/(.|\n)+\n(.|\n)*$";
 
-    public int add(String numbers)
+    public int add(String str)
     {
-        if (numbers.strip().equals(""))
-            return 0;
-        else if (!numbers.contains(DELIMITER))
-            return Integer.parseInt(numbers.strip());
-        else if (numbers.split(DELIMITER).length >= 2)
+        int numberStartIndex = getNumberStartIndex(str);
+
+        String[] delimiterArray;
+        String numbers;
+
+        String delimiterRegex;
+        String[] numberArray;
+
+        if (numberStartIndex == -1)
         {
-            return Arrays.stream(numbers.split(DELIMITER))
+            return 0;
+        }
+
+        if (str.matches(DELIMITER_REGEX))
+        {
+            var delimiters = getDelimitersForRegex(str, numberStartIndex);
+
+            delimiterArray = getDelimiterArray(delimiters);
+            numbers = str.substring(numberStartIndex).strip();
+        }
+        else {
+            delimiterArray = new String[] {"\n", ","};
+            numbers = str.strip();
+        }
+
+        delimiterRegex = getDelimiterRegexForArray(delimiterArray);
+        numberArray = numbers.split(delimiterRegex);
+
+        // Case handling
+        if (numbers.equals(""))
+        {
+            return 0;
+        }
+        else if (numberArray.length == 1)
+        {
+            var number = Integer.parseInt(numbers);
+            return isValidNumber(number) ? number : -1;
+        }
+        else if (numberArray.length >= 2)
+        {
+            return Arrays.stream(numberArray)
                     .map(String::strip)
                     .filter(number -> !number.isBlank())
                     .mapToInt(Integer::parseInt)
+                    .filter(this::isValidNumber)
                     .sum();
         }
         else
-            return 999;
+        {
+            return -1;
+        }
+    }
+
+    private int getNumberStartIndex(String str)
+    {
+        int numberStartIndex = -1;
+        int returnIndex = str.indexOf("\n");
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (Character.isDigit(str.charAt(i)) && i > returnIndex)
+            {
+                numberStartIndex = i;
+                break;
+            }
+        }
+
+        return numberStartIndex;
+    }
+
+    private String getDelimitersForRegex(String str, int numberStartIndex)
+    {
+        String delimiters = str.substring(2, numberStartIndex - 1).strip();
+
+        if (delimiters.isEmpty()) {
+            delimiters = Character.toString(str.charAt(2));
+        }
+
+        return delimiters;
+    }
+
+    private String[] getDelimiterArray(String delimiters)
+    {
+        if (delimiters.contains("]["))
+        {
+            return delimiters.substring(1, delimiters.length() - 1).split("]\\[");
+        }
+        else
+        {
+            return new String[] {delimiters};
+        }
+    }
+
+    private String getDelimiterRegexForArray(String[] delimiters)
+    {
+        List<String> delimiterList = Arrays.stream(delimiters)
+                .filter(d -> !d.isBlank())
+                .map(Pattern::quote)
+                .collect(Collectors.toList());
+
+        return "(" + String.join("|", delimiterList) + ")+";
+    }
+
+    public boolean isValidNumber(int num)
+    {
+        return num <= 1000;
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -38,12 +38,44 @@ public class StringCalculatorTest
     public void addWithValidIntTestMultipleIntegers()
     {
         Assertions.assertEquals(1337, stringCalculator.add("44,346,2,945"));
+        Assertions.assertEquals(138, stringCalculator.add("123, ,15"));
     }
 
     @Test
-    public void addWithInvalidInt()
+    public void addWithValidIntTestReturnDelimiter()
     {
-        Assertions.assertEquals(999, stringCalculator.add("25,91,16"));
-        Assertions.assertEquals(999, stringCalculator.add("123,,15"));
+        Assertions.assertEquals(101, stringCalculator.add("44,32\n25"));
+    }
+
+    @Test
+    public void addWithValidIntTestAnyDelimiter()
+    {
+        Assertions.assertEquals(101, stringCalculator.add("//&\n44&32&25"));
+        Assertions.assertEquals(101, stringCalculator.add("//\n\n44\n32\n25"));
+        Assertions.assertEquals(101, stringCalculator.add("//\n\n\n \r44\n32\n25\n \r"));
+        Assertions.assertEquals(101, stringCalculator.add("//0\n044032025"));
+    }
+
+    @Test
+    public void addWithValidIntTestNumberRange() {
+        Assertions.assertEquals(-1, stringCalculator.add("//[X]\n1001"));
+        Assertions.assertEquals(6, stringCalculator.add("//+\n1+2+3+" + 2147483647));
+    }
+
+    @Test
+    public void addWithValidIntTestAnyLengthDelimiter() {
+        Assertions.assertEquals(101, stringCalculator.add("//<DELIMITER>\n44<DELIMITER>32<DELIMITER>25"));
+    }
+
+    @Test
+    public void addWithValidIntTestAnyMultipleDelimiters() {
+        Assertions.assertEquals(166, stringCalculator.add("//[0-0][|][:]\n1:2:3:4|50-06|70-08:9:100-0110-0100"));
+        Assertions.assertEquals(14, stringCalculator.add("//[1][][]\n012131415"));
+    }
+
+    @Test
+    public void addWithInvalidDelimiterAny()
+    {
+        Assertions.assertThrows(NumberFormatException.class, () -> stringCalculator.add("//\n\n1;2;3"));
     }
 }
