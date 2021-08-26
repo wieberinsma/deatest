f073db0b78e20a7f1c4eb1677c90d1a78e3e01d3
Wiebe Rinsma <wiebe.rinsma@han.nl>
2021-08-26 15:43:15 +0200

    Start from OOSE

---
+++ src/main/java/http/HttpServer.java
@@ -0,0 +1,34 @@
+package http;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class HttpServer {
+
+    private int tcpPort;
+
+    public HttpServer(int tcpPort) {
+        this.tcpPort = tcpPort;
+    }
+
+    public static void main(String[] args) {
+        new HttpServer(8383).startServer();
+    }
+
+    public void startServer() {
+        try (
+                var serverSocket = new ServerSocket(this.tcpPort);
+        ) {
+            System.out.println("Server accepting requests on port " + tcpPort);
+
+            while (!serverSocket.isClosed()) {
+                var acceptedSocket = serverSocket.accept();
+                var connectionHandler = new ConnectionHandler(acceptedSocket);
+                var connection = new Thread(connectionHandler::handle);
+                connection.start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
